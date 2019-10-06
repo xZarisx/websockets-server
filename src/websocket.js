6882a74145cadd8e1c04130526d1ac3c7671b57a
Matt Poulson <mpoulson@nav.com>
2019-10-05 18:46:29 -0600

    add prittere and game map

--- src/websocket.js
+++ src/websocket.js
@@ -1,23 +1,45 @@
+import uuid from 'uuid'
 import expressWs from './express-ws'
+import store from './game-state/store'
+import {
+  movePlayerUp,
+  movePlayerDown,
+  movePlayerLeft,
+  movePlayerRight,
+  addPlayer,
+  removePlayer
+} from './game-state/map'
+console.log('store.getState().players: ', store.getState().players)
 
-let messages = []
+const aWss = expressWs.getWss('/websocket')
 
-const aWss = expressWs.getWss('/websocket');
+store.subscribe(() => {
+  aWss.clients.forEach((client) => {
+    client.send(JSON.stringify(store.getState().map))
+  })
+})
 
 const websocket = (ws, req) => {
-  ws.on('open', (msg) => {
-    messages = [...messages, 'welcome']
-    ws.send(JSON.stringify(messages));
-  });
+  ws.id = uuid.v4()
+  store.dispatch(addPlayer(ws.id))
   ws.on('message', (msg) => {
-    messages = [...messages, msg]
-    aWss.clients.forEach((client) => {
-      client.send(JSON.stringify(messages));
-    });
-  });
+    switch (msg) {
+      case 'up':
+        store.dispatch(movePlayerUp(ws.id))
+        return
+      case 'down':
+        store.dispatch(movePlayerDown(ws.id))
+        return
+      case 'left':
+        store.dispatch(movePlayerLeft(ws.id))
+        return
+      case 'right':
+        store.dispatch(movePlayerRight(ws.id))
+    }
+  })
   ws.on('close', (msg) => {
-    messages = [...messages, `good bye`]
-  });
+    store.dispatch(removePlayer(ws.id))
+  })
 }
 
 export default websocket
